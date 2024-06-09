ffcd5a9a9708d9b9ab9f33c0fa24a124a490744a
Mukesh Pandey <panditmukki5@gmail.com>
2024-06-09 21:31:42 +0530

    Home Page Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,14 @@ import {
 import "./App.css";
 import Layout from "./Layout";
 import HomePage from "./Pages/HomePage";
+import KPIDetails from "./Pages/KPIDetails";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route path="" element={<HomePage />} />
+        <Route path="/gp-profile/:id" element={<KPIDetails />} />
       </Route>
     )
   );
