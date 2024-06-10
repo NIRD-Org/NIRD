c8170ee33841b29b8f0860dd54b82ca208e38ba4
Mukesh Pandey <panditmukki5@gmail.com>
2024-06-10 21:24:52 +0530

    Home Page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <section class="relative bg-cover bg-center h-screen">
-        <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
+        <div class="absolute inset-0 h-[100vh] bg-gray-900 opacity-50"></div>
         <div class="relative w-full mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-center h-full">
           <div className="flex flex-1">
             <div className="flex-1">
@@ -12,33 +12,34 @@ function Home() {
                 Empowering Communities. Transforming Lives.
               </h1>
               <p class="mt-4 text-white text-lg md:text-xl">
-                Join us in enhancing lives through sustainable development and empowering
-                communities across India. Together, we can make a lasting impact.
+                Join us in enhancing lives through sustainable development and
+                empowering communities across India. Together, we can make a
+                lasting impact.
               </p>
               <a
                 href="#learn-more"
-                class="mt-6 inline-block bg-blue-600 text-white px-6 py-3 rounded-md text-lg font-medium hover:bg-blue-700"
+                class="mt-6 inline-block bg-[#004B86] text-white px-6 py-3 rounded-md text-lg font-medium hover:bg-blue-900"
               >
                 Learn More
               </a>
             </div>
             <div className="flex-1"></div>
             <img
-              className="absolute w-full top-0 right-0 left-0 z-[-10] bg-opacity-30 h-[90%] object-cover"
+              className="absolute w-full top-0 right-0 left-0 z-[-10] bg-opacity-30 h-[100%] object-cover"
               src="https://res.cloudinary.com/dtbbuevez/image/upload/v1717957901/themes/hfaiqrqfcbu0lo8pmkvz.jpg"
               alt=""
             />
           </div>
         </div>
       </section>
-      <section>
+      {/* <section>
         <div className="flex gap-10 items-center bg-transparent">
           <img className="w-40 mix-blend-multiply" src="/logo/LSDG Logo.png" alt="" />
           <div>
             <Themes />
           </div>
         </div>
-      </section>
+      </section> */}
     </div>
   );
 }
@@ -72,14 +73,15 @@ const Themes = () => {
       theme: "Low cost voluntary activities",
     },
     {
-      imgUrl: "https://res.cloudinary.com/dtbbuevez/image/upload/v1717959547/4_1_tnvg14.jpg",
+      imgUrl:
+        "https://res.cloudinary.com/dtbbuevez/image/upload/v1717959547/4_1_tnvg14.jpg",
       theme: "Good practices and achievements",
     },
   ];
 
   return (
     <div className="flex gap-5 items-center overflow-auto lg:flex-wrap  lg:overflow-hidden">
-      {images.map(d => (
+      {images.map((d) => (
         <ThemeCard imgUrl={d.imgUrl} theme={d.theme} />
       ))}
     </div>
