b2c4a19c3ff40a032def7d49cc12cbbf4da69b7e
Anoop Singh <anoop102910@gmail.com>
2024-06-17 11:21:42 +0530

    fix attendance

--- frontend/src/components/admin/attendance/Attendance.jsx
+++ frontend/src/components/admin/attendance/Attendance.jsx
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Link, useSearchParams } from "react-router-dom";
 import StateFilter from "@/components/admin/filter/StateFilter";
 import DistrictFilter from "@/components/admin/filter/DistrictFilter";
 import BlockFilter from "@/components/admin/filter/BlockFilter";
 import AdminHeader from "../AdminHeader";
 
 const Attendance = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const state_id = searchParams.get("state_id") || "";
-  const dist_id = searchParams.get("dist_id") || "";
-  const block_id = searchParams.get("block_id") || "";
-  const [gp, setGrams] = useState([]);
-
   return (
     <div className="container mx-auto p-4">
       <AdminHeader>All attendance</AdminHeader>
