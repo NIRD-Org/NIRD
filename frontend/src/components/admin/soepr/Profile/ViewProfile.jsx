97f819c3d3efeed3e814590ddedb45bfcbce0f1c
GouthamiArun <gouthamivoorugonda@gmail.com>
2024-07-31 11:56:49 +0530

    view profile layout changes

--- frontend/src/components/admin/soepr/Profile/ViewProfile.jsx
+++ frontend/src/components/admin/soepr/Profile/ViewProfile.jsx
@@ -5,8 +5,9 @@ import { useAuthContext } from "@/context/AuthContext";
 
 function ViewProfile() {
   const { user } = useAuthContext();
-  const [profileData, setProfileData] = useState();
+  const [profileData, setProfileData] = useState({});
   const [photo, setPhoto] = useState(null);
+
   useEffect(() => {
     // Fetch user profile data
     const fetchProfileData = async () => {
@@ -26,95 +27,64 @@ function ViewProfile() {
 
   return (
     <div className="w-full flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-4xl p-6 bg-white rounded-md shadow-md">
-        <h2 className="text-2xl font-semibold mb-6 text-center">
-          VIEW PROFILE
-        </h2>
-        <div className="space-y-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-              <p>
-                <strong>Full Name:</strong> {profileData?.name}
-              </p>
-            </div>
-            <div>
-              <p>
-                <strong>Gender:</strong> {profileData?.gender}
-              </p>
-            </div>
-          </div>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-              <p>
-                <strong>Email ID:</strong> {profileData?.email}
-              </p>
-            </div>
-            <div>
-              <p>
-                <strong>Mobile No.:</strong> {profileData?.mobile}
-              </p>
-            </div>
-          </div>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-              <p>
-                <strong>Qualification:</strong>{" "}
-                {profileData?.qualifications || "No Data Available"}
-              </p>
-            </div>
-            <div>
-              <p>
-                <strong>Date of Joining:</strong> {profileData?.dojNIRDPR}
-              </p>
-            </div>
-          </div>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-              <p>
-                <strong>Deployed State:</strong> {profileData?.state}
-              </p>
-            </div>
-            <div>
-              <p>
-                <strong>Area of Expertise:</strong>{" "}
-                {profileData?.areaOfExpertise || "No Data Available"}
-              </p>
-            </div>
-          </div>
-
-          <div>
+      <div className="w-full max-w-4xl px-10 py-8 bg-white rounded-md shadow-md">
+        <h2 className="text-2xl font-semibold mb-6 text-center">SOEPR - EMPLOYEE PROFILE</h2>
+        <div className="flex flex-col md:flex-row md:items-start space-y-6 md:space-y-0 md:space-x-2">
+          <div className="flex-grow space-y-4 p-6">
             <p>
-              <strong>Designation:</strong> {profileData?.designation}{" "}
-              {/* Display designation */}
+              <strong>Full Name:</strong> {profileData.name}
+            </p>
+            <p>
+              <strong>Designation:</strong> {profileData.designation}
+            </p>
+            <p>
+              <strong>Deployed State:</strong> {profileData.state}
+            </p>
+            <p>
+              <strong>Date of Joining:</strong> {profileData.dojNIRDPR}
+            </p>
+            <p>
+              <strong>Gender:</strong> {profileData.gender}
+            </p>
+            <p>
+              <strong>Email ID:</strong> {profileData.email}
+            </p>
+            <p>
+              <strong>Mobile No.:</strong> {profileData.mobile}
+            </p>
+            <p>
+              <strong>Qualification:</strong> {profileData.qualifications || "No Data Available"}
             </p>
           </div>
-
-          <div>
-            <p>
-              <strong>Professional Photograph:</strong>
-            </p>
-            {profileData?.photo ? (
-              <img
-                src={profileData.photo}
-                alt="Professional"
-                className="max-w-xs rounded-md border"
-              />
-            ) : (
-              <p>No photograph uploaded.</p>
-            )}
+          <div className="flex-shrink-0 p-6">
+            <div className="flex flex-col items-center">
+              
+              <br/>
+              {profileData.photo ? (
+                <img
+                  src={profileData.photo}
+                  alt="Professional"
+                  className="w-36 h-48 rounded-md border mb-4"
+                />
+              ) : (
+                <p>No photograph uploaded.</p>
+              )}
+              <div>
+                <p>
+                  <strong>Area of Expertise:</strong>{" "}
+                  {profileData.areaOfExpertise || "No Data Available"}
+                </p>
+              </div>
+            </div>
           </div>
-
-          <div className="flex justify-center mt-6">
-            <Link
-              to="/admin/soepr/profile-form"
-              className="px-4 py-2 text-sm text-white bg-primary rounded-md hover:bg-sky-950"
-            >
-              Edit Profile
-            </Link>
-          </div>
+        </div>
+        <div className="flex justify-center mt-6">
+          <Link
+            to="/admin/soepr/profile-form"
+            className="px-4 py-2 text-sm text-white bg-primary rounded-md hover:bg-sky-950"
+          >
+            Edit Profile
+          </Link>
         </div>
       </div>
     </div>
