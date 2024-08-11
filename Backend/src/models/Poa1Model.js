7a33f76388a4fabd41e7dd41fd23bf16296a0bdf
Mukesh Pandey <panditmukki5@gmail.com>
2024-08-11 19:06:27 +0530

    poa1 apis

--- Backend/src/models/Poa1Model.js
+++ Backend/src/models/Poa1Model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const POA1DaySchema = new mongoose.Schema(
   {
     date: {
-      type: Date,
+      type: String,
       required: true,
     },
     weekday: {
--- Backend/src/routes/poa1Routes.js
+++ Backend/src/routes/poa1Routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { isAuth } from "../middlewares/auth.js";
-import { createPoa1 } from "../controllers/Poa1Controller.js";
+import { createPoa1, getPoa1Data } from "../controllers/Poa1Controller.js";
 
 const router = express.Router();
 router.route("/create").post(isAuth, createPoa1);
+router.route("/").get(getPoa1Data);
 
 export default router;
